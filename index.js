1970ab2c41d0cb0321393f3e4fdf5bebd2271fbf
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-19 19:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> 508aee27b14c50d487ba134ce9fcd0a42c84cd86
 // ** 1
 
 function happyHolidays(string) {
   return "Happy holidays!"
 }
 
+<<<<<<< HEAD
 // ** 2
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
@@ -20,3 +24,15 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
+=======
+// **2
+
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+  var = 'you'
+}
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
+>>>>>>> 508aee27b14c50d487ba134ce9fcd0a42c84cd86
